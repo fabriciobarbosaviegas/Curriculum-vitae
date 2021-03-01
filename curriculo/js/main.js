329fc58272263bfd92e0edda7725d0acffeeb543
Fabrício Barbosa Viegas <incy8498@gmail.com>
2021-03-01 08:59:40 -0300

    Add files via upload

---
+++ curriculo/js/main.js
@@ -0,0 +1,94 @@
+// Initialising the canvas
+var canvas = document.querySelector('canvas'),
+    ctx = canvas.getContext('2d');
+
+// Setting the width and height of the canvas
+canvas.width = window.innerWidth;
+canvas.height = 1920;
+
+// Setting up the letters
+var letters = 'MECONTRATA!CONTRACTME!MECONTRATA!CONTRACTME!MECONTRATA!CONTRACTME!MECONTRATA!CONTRACTME!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!CONTRACTME!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!MECONTRATA!';
+letters = letters.split('');
+
+// Setting up the columns
+var fontSize = 10,
+    columns = canvas.width / fontSize;
+
+var drops = [];
+for (var i = 0; i < columns; i++) {
+  drops[i] = 1;
+}
+
+function draw() {
+  ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < drops.length; i++) {
+    var text = letters[Math.floor(Math.random() * letters.length)];
+    ctx.fillStyle = '#0f0';
+    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+    drops[i]++;
+    if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+      drops[i] = 0;
+    }   
+  }
+}
+
+
+setInterval(draw, 33);
+
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+       if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        } 
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
