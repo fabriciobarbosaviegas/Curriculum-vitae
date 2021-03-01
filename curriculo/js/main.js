817ead0a6f670ec992d4c04646b63ca12bafedcb
Fabrício Barbosa Viegas <incy8498@gmail.com>
2021-03-01 12:00:02 -0300

    Update main.js

--- curriculo/js/main.js
+++ curriculo/js/main.js
@@ -1,3 +1,33 @@
+//active menu
+var sections = document.querySelectorAll("section");
+
+onscroll = function () {
+  var scrollPosition = document.documentElement.scrollTop;
+
+  sections.forEach((section) => {
+    if (
+      scrollPosition >= section.offsetTop - section.offsetHeight * 0.25 &&
+      scrollPosition <
+        section.offsetTop + section.offsetHeight - section.offsetHeight * 0.25
+    ) {
+      var currentId = section.attributes.id.value;
+      removeAllActiveClasses();
+      addActiveClass(currentId);
+    }
+  });
+};
+
+var removeAllActiveClasses = function () {
+  document.querySelectorAll("div li a").forEach((el) => {
+    el.classList.remove("active");
+  });
+};
+
+var addActiveClass = function (id) {
+  var selector = `div li a[href="#${id}"]`;
+  document.querySelector(selector).classList.add("active");
+};
+
 // Initialising the canvas
 var canvas = document.querySelector('canvas'),
     ctx = canvas.getContext('2d');
